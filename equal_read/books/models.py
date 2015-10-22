f176efd07284b00957bcd4e1da75ba5904564993
Yiyang Shen <yshen@berkeley.edu>
2015-10-22 16:10:00 -0700

    model field filled

--- equal_read/books/models.py
+++ equal_read/books/models.py
@@ -30,30 +30,30 @@ from django.db import models
 
 
 class Book(models.Model):
-    listTitle = models.CharField(maxlength=1000)
+    listTitle = models.CharField(max_length=1000)
     title = models.CharField(max_length=1000)
-    source = 
-    isbn = 
-    author = 
-    illustrator = 
-    publisher = 
-    year = 
-    pages = 
-    readingLevel = 
-    gradeLevel = 
-    nameOfReader = 
-    gender = 
-    genderExpression = 
-    age = 
-    ethnicity = 
-    disability = 
-    sexualOrientation = 
-    education = 
-    income = 
-    religion = 
-    geography = 
-    familyStatus = 
-    notesByReader = 
+    source = models.CharField(max_length=1000) 
+    isbn = models.BigIntegerField(default=0)
+    author = models.CharField(max_length=1000)
+    illustrator = models.CharField(max_length=1000)
+    publisher = models.CharField(max_length=1000)
+    year = models.DateField()
+    pages = models.PositiveIntegerField()
+    readingLevel = models.CharField(max_length=1000)
+    gradeLevel = models.CharField(max_length=1000)
+    nameOfReader = models.CharField(max_length=1000)
+    gender = models.CharField(max_length=1000)
+    genderExpression = models.CharField(max_length=1000)
+    age = models.PositiveSmallIntegerField(default=0)
+    ethnicity = models.CharField(max_length=1000)
+    disability = models.CharField(max_length=1000)
+    sexualOrientation = models.CharField(max_length=1000)
+    education = models.CharField(max_length=1000)
+    income = models.CharField(max_length=1000)
+    religion = models.CharField(max_length=1000)
+    geography = models.CharField(max_length=1000)
+    familyStatus = models.CharField(max_length=1000)
+    notesByReader = models.CharField(max_length=1000)
 
     # import @python_2_unicode_compatible if desired
     def __str__(self):
